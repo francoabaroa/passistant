998888f93f3512cfb5cf8223c076f8f907855c9e
Franco Abaroa <franco9395@gmail.com>
2023-05-25 11:37:00 -0400

    Add boilerplate for data processing functionality

--- backend/app/services/data_ingestion_service.py
+++ backend/app/services/data_ingestion_service.py
@@ -1,7 +1,11 @@
 from flask import request
 from twilio.twiml.messaging_response import MessagingResponse
+from .data_processing_service import DataProcessingService
 
 class DataIngestionService:
+    def __init__(self):
+        self.data_processor = DataProcessingService()
+
     def ingest_sms(self):
         resp = MessagingResponse()
         msg = request.form.get('Body')  # Extract the message body from the incoming request.
@@ -10,14 +14,19 @@ class DataIngestionService:
 
         if media_type is None:
             response_text = "You sent a text message: {}".format(msg)
+            self.data_processor.process_text(msg)
         elif 'audio' in media_type:
             response_text = "You sent a voice note: {}".format(media_url)
+            self.data_processor.process_audio(media_url)
         elif 'image' in media_type:
             response_text = "You sent an image: {}".format(media_url)
+            self.data_processor.process_image(media_url)
         elif 'application/pdf' in media_type:
             response_text = "You sent a PDF: {}".format(media_url)
+            self.data_processor.process_document(media_url)
         else:
             response_text = "You sent an unsupported file type: {}".format(media_url)
+            self.data_processor.process_unsupported(media_url)
 
         resp.message(response_text)
         print(response_text)
--- backend/app/services/data_processing_service.py
+++ backend/app/services/data_processing_service.py
@@ -1,1 +1,23 @@
-# Uses OpenAI's and other external/internal tools to parse and process the ingested data, extracting relevant information, saving it and creating relevant events (calendar, sms, etc)
+class DataProcessingService:
+    def __init__(self):
+        pass
+
+    def process_text(self, msg):
+        # Your text processing code goes here
+        print("Processing text: {}".format(msg))
+
+    def process_audio(self, media_url):
+        # Your audio processing code goes here
+        print("Processing audio from: {}".format(media_url))
+
+    def process_image(self, media_url):
+        # Your image processing code goes here
+        print("Processing image from: {}".format(media_url))
+
+    def process_document(self, media_url):
+        # Your document processing code goes here
+        print("Processing document from: {}".format(media_url))
+
+    def process_unsupported(self, media_url):
+        # Code to handle unsupported media types goes here
+        print("Cannot process unsupported file type from: {}".format(media_url))
