015ebacd590e156a12c539584bdbc1f333c70fad
Franco Abaroa <franco9395@gmail.com>
2023-05-29 16:32:11 -0400

    Clean up code

--- backend/app/services/data_ingestion_service.py
+++ backend/app/services/data_ingestion_service.py
@@ -22,16 +22,14 @@ class DataIngestionService:
         media_url = request.form.get('MediaUrl0')  # Extract the media URL from the incoming request.
 
         if media_type is None:
-            response_text = "You sent a text message: {}".format(msg)
-            dict = self.data_processor.process_text(msg)
-            self.send_response_sms(dict)
+            response = self.data_processor.process_text(msg)
+            self.send_response_sms(response)
         elif 'audio' in media_type:
-            response_text = "You sent a voice note: {}".format(media_url)
-            dict = self.data_processor.process_audio(media_url)
-            self.send_response_sms(dict)
+            response = self.data_processor.process_audio(media_url)
+            self.send_response_sms(response)
         elif 'image' in media_type:
-            response_text = "You sent an image: {}".format(media_url)
-            self.data_processor.process_image(media_url)
+            response = self.data_processor.process_image(media_url)
+            self.send_response_sms(response)
         elif 'application/pdf' in media_type:
             response_text = "You sent a PDF: {}".format(media_url)
             self.data_processor.process_document(media_url)
@@ -42,11 +40,10 @@ class DataIngestionService:
         resp.message(response_text)
         return str(resp)
 
-    def send_response_sms(self, dict):
+    def send_response_sms(self, body):
         account_sid = TWILIO_ACCOUNT_SID
         auth_token = TWILIO_AUTH_TOKEN
         client = Client(account_sid, auth_token)
-        body = "\n".join(f"{key}: {value}" for key, value in dict.items())
 
         message = client.messages.create(
             to=TWILIO_TO_NUMBER,
