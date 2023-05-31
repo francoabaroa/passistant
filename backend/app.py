0fa9af90f2f7bad9d8ab39cda204d0b202683675
Franco Abaroa <franco9395@gmail.com>
2023-05-30 22:00:29 -0400

    Fix multiple instances issue

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from app.routes import register_blueprints
+from app import db
 from config import Config
 from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
 
-# Initialize database and migrate objects
-db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app():
@@ -15,10 +13,8 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
 
-    # Import the models
     from app.models import User, Family, Reminder, List, ListItem
 
-    # Create the database tables
     with app.app_context():
         db.create_all()
 
