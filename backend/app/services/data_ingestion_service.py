f94a8d39a6f884572710e514d432b1059298ef14
Franco Abaroa <franco9395@gmail.com>
2023-06-01 12:37:56 -0400

    Respond with clarification message or response message

--- backend/app/services/data_ingestion_service.py
+++ backend/app/services/data_ingestion_service.py
@@ -1,4 +1,5 @@
 from flask import request, jsonify
+import json
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
 from .data_processing_service import DataProcessingService
@@ -20,16 +21,20 @@ class DataIngestionService:
         msg = request.form.get('Body')  # Extract the message body from the incoming request.
         media_type = request.form.get('MediaContentType0')  # Extract the media type from the incoming request.
         media_url = request.form.get('MediaUrl0')  # Extract the media URL from the incoming request.
+        response = ""
 
         if media_type is None:
             response = self.data_processor.process_text(msg)
-            self.send_response_sms(response)
+            reply_message = self.get_reply_message(response)
+            self.send_response_sms(reply_message)
         elif 'audio' in media_type:
             response = self.data_processor.process_audio(media_url)
-            self.send_response_sms(response)
+            reply_message = self.get_reply_message(response)
+            self.send_response_sms(reply_message)
         elif 'image' in media_type:
             response = self.data_processor.process_image(media_url)
-            self.send_response_sms(response)
+            reply_message = self.get_reply_message(response)
+            self.send_response_sms(reply_message)
         elif 'application/pdf' in media_type:
             response = "You sent a PDF: {}".format(media_url)
             self.data_processor.process_document(media_url)
@@ -39,6 +44,13 @@ class DataIngestionService:
 
         resp.message(response)
         return str(resp)
+
+    def get_reply_message(self, response):
+        json_response = json.loads(response)
+        clarification_needed = json_response.get('clarificationNeeded')
+        clarification_message = json_response.get('clarificationMessage')
+        response_message = json_response.get('responseMessage')
+        return clarification_message if clarification_needed else response_message
 
     def send_response_sms(self, body):
         account_sid = TWILIO_ACCOUNT_SID
