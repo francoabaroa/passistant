55b9c5cb65b02b1e860829bb16e70760c805a7db
Franco Abaroa <franco9395@gmail.com>
2023-06-02 13:19:33 -0400

    [nit]: Fix spacing

--- backend/app/services/data_retrieval_service.py
+++ backend/app/services/data_retrieval_service.py
@@ -7,19 +7,19 @@ class DataRetrievalService:
         pass
 
     def get_all_lists(self, from_number):
-      lists = db.session.query(List).join(Member).filter(
-          and_(Member.phone == from_number, List.member_id == Member.id)
-      ).all()
+        lists = db.session.query(List).join(Member).filter(
+            and_(Member.phone == from_number, List.member_id == Member.id)
+        ).all()
 
-      all_lists = [
-          {
-              'name': list.name,
-              'items': [{'name': item.name, 'quantity': item.quantity} for item in list.list_items]
-          }
-          for list in lists
-      ]
+        all_lists = [
+            {
+                'name': list.name,
+                'items': [{'name': item.name, 'quantity': item.quantity} for item in list.list_items]
+            }
+            for list in lists
+        ]
 
-      return all_lists
+        return all_lists
 
     def get_all_reminder_names(self, from_number):
         reminders = db.session.query(Reminder).join(Member).filter(
