5e09d12e4a77dfafc79b3844dd8cae937af9fa9e
Franco Abaroa <franco9395@gmail.com>
2023-06-02 19:06:17 -0400

    Add home page title

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,8 +4,10 @@ export default function Home() {
 
   return (
     <>
-      <main className="flex flex-col items-center justify-between p-24 min-h-screen">
-        <div className="z-10 max-w-5xl w-full items-start justify-between font-mono text-sm flex">
+      <main className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold mb-4">Welcome to smilo!</h1>
+          <p className="text-xl">Your AI coparent assistant</p>
         </div>
       </main>
     </>
