82c760c8355d8fba19eb84e9cf452fd94bac6b9c
Franco Abaroa <franco9395@gmail.com>
2023-06-02 18:22:34 -0400

    Add styling to not found page

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -1,12 +1,19 @@
-import Link from 'next/link';
-
 export default function NotFound() {
   return (
-    <div className="text-center mt-10">
-      <h2>Not Found</h2>
-      <p>
-      <Link href="/">Go Home</Link>
-      </p>
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full">
+        <div className="text-center">
+          <h2 className="mt-2 text-4xl font-extrabold text-gray-900">
+            Page Not Found
+          </h2>
+          <p className="mt-2 text-base text-gray-500">Sorry, we couldn’t find the page you’re looking for.</p>
+          <div className="mt-6">
+            <div className="hidden lg:block lg:ml-10">
+              <a href="/" className="text-base font-medium text-indigo-600 hover:text-indigo-500">Go back home<span aria-hidden="true"> &rarr;</span></a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
