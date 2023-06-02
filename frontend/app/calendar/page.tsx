502c0c55a85d7e3e82fe452b54e62ca015a6cbd2
Franco Abaroa <franco9395@gmail.com>
2023-06-02 18:53:19 -0400

    Add coming soon for calendar and settings

---
+++ frontend/app/calendar/page.tsx
@@ -0,0 +1,13 @@
+export default function Calendar() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full">
+        <div className="text-center">
+          <h2 className="mt-2 text-4xl font-extrabold text-gray-900">
+            Coming Soon
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+}
